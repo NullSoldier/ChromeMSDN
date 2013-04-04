d79ed0db1b3b059f3f985ddcc7d0f9b0873b5212
Jason Spafford <nullprogrammer@gmail.com>
2013-04-04 14:23:08 -0700

    Reduced permission scope to only ask for msdn
    It now displays an error if you try to use it on a page other than
    msdn.microsoft.com

--- src/background.js
+++ src/background.js
@@ -1,3 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+	if (tab.url.indexOf("msdn.microsoft.com") == -1) {
+		alert ("Extension is only usable on msdn.microsoft.com");
+		return;
+	}
 	chrome.tabs.executeScript(null, {file:"content.js"});
 });
