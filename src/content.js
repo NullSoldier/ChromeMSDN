fb1522d4cd0e0e001c85863296fffe07566b3670
Todor Vyagov <rambo007@abv.bg>
2016-09-12 18:32:40 +0300

    Update content.js

--- src/content.js
+++ src/content.js
@@ -1,6 +1,8 @@
 Array.prototype.forEach.call (
-	document.querySelectorAll ('tr[data*=inherited]'),
+	document.querySelectorAll ('tr'),
 	function (node) {
-		node.style.display = node.style.display === 'none' ? '' : 'none';
+		if(node.innerHTML.indexOf('(Inherited from') != -1){
+			node.style.display = node.style.display === 'none' ? '' : 'none';
+		}
 	}
 )
